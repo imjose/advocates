201b6e1b139a55dfb64d58a450ff8ac735ea8859
Jose <jose@unioncredit.app>
2025-09-26 17:39:15 -0400

    api request now considers searchParams & pagination logic.

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,9 +1,25 @@
-import db from "../../../db";
+import db from '@/db';
+
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { count } from 'drizzle-orm';
 
-export async function GET() {
-  const data = await db.select().from(advocates);
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
 
-  return Response.json({ data });
+  const limit = searchParams.has("pageSize") ? parseInt(searchParams.get("pageSize") as string, 10) : 10;
+  const offset = searchParams.has("page") && limit
+    ? (parseInt(searchParams.get("page") as string, 10) - 1) * limit
+    : 0;
+
+  const total = await db
+    .select({ count: count() })
+    .from(advocates);
+  
+  const data = await db
+    .select()
+    .from(advocates)
+    .limit(limit)
+    .offset(offset);
+
+  return Response.json({ data, totalItems: total[0].count });
 }
