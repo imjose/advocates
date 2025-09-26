0515d5e2d8c502c654bf1fc784a8a5dc973c9cf1
Jose <jose@unioncredit.app>
2025-09-26 17:37:54 -0400

    pagination configuration added to proper fetch table elements considering page# & size & useAdvocates are now being used to fetch data.

--- src/app/advocates/page.tsx
+++ src/app/advocates/page.tsx
@@ -1,56 +1,56 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import { Advocate } from '@/lib/types/advocate';
 import AdvocatesTable from '@/lib/components/advocatesTable';
 import SearchBar from '@/lib/components/searchBar';
-import { Header } from 'antd/es/layout/layout';
+import { useAdvocates } from '@/lib/hooks/useAdvocates';
+import { PaginationOptions } from '@/lib/types/pagination';
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState<Advocate[]>([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
+  const [paginationOptions, setPaginationOptions] = useState<PaginationOptions>({ page: 1, pageSize: 10 });
+  
+  const { data: advocates, loading } = useAdvocates(paginationOptions);
 
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
-  }, []);
+  const onPageChangeCallback = (page: number, pageSize: number) => {
+    setPaginationOptions({ page, pageSize });
+  };
 
   const onChange = (e) => {
-    const searchTerm = e.target.value;
+    // const searchTerm = e.target.value;
 
-    document.getElementById("search-term").innerHTML = searchTerm;
+    // document.getElementById("search-term").innerHTML = searchTerm;
 
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
+    // console.log("filtering advocates...");
+    // const filteredAdvocates = advocates.filter((advocate) => {
+    //   return (
+    //     advocate.firstName.includes(searchTerm) ||
+    //     advocate.lastName.includes(searchTerm) ||
+    //     advocate.city.includes(searchTerm) ||
+    //     advocate.degree.includes(searchTerm) ||
+    //     advocate.specialties.includes(searchTerm) ||
+    //     advocate.yearsOfExperience.includes(searchTerm)
+    //   );
+    // });
 
-    setFilteredAdvocates(filteredAdvocates);
+    // setFilteredAdvocates(filteredAdvocates);s
   };
 
   const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
+    // console.log(advocates);
+    // setFilteredAdvocates(advocates);
   };
 
   return (
     <main className='my-6 mx-12'>
         <div className='flex flex-col items-start gap-4'>
-            <SearchBar onChange={onChange} onClick={onClick} />
-            <AdvocatesTable data={filteredAdvocates} loading={!filteredAdvocates.length} />
+            <SearchBar onChange={onChange} onClick={onClick} loading={loading} />
+            <AdvocatesTable 
+                tableData={advocates} 
+                loading={loading} 
+                paginationOptions={paginationOptions} 
+                onPageChange={onPageChangeCallback} 
+            />
         </div>
     </main>
   );
--- src/lib/components/searchBar.tsx
+++ src/lib/components/searchBar.tsx
@@ -7,23 +7,25 @@ const { Search } = Input;
 type Props = {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     onClick: () => void;
+    loading: boolean;
 }
 
-function SearchBar({ onChange, onClick }: Props) {
+function SearchBar({ onChange, onClick, loading }: Props) {
   return (
     <div className='inline-flex flex-row gap-1'>
         <Search 
           aria-label="Search advocates"
           placeholder="input search text" 
           enterButton="Search" 
-          size="middle" 
-          loading
+          size="middle"
+          disabled={loading}
           onChange={onChange} 
         />
         <Button 
           aria-label="Reset search"
           variant='solid' 
-          color='danger' 
+          color='danger'
+          disabled={loading}
           onClick={onClick}
         > Reset Search
         </Button>
