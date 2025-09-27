1a6b08bd8b5ed1565040bda79a208dc631a54f83
Jose <jose@unioncredit.app>
2025-09-27 09:40:00 -0400

    general method descriptions added

--- src/lib/util/params.ts
+++ src/lib/util/params.ts
@@ -1,3 +1,8 @@
+/**
+ * Resolves search parameters from a generic object into URLSearchParams
+ * @param options - (optional) generic object containing key-value pairs to be converted to URLSearchParams
+ * @returns URLSearchParams
+ */
 export function resolveSearchParams<T>(options?: T): URLSearchParams {
     const searchParams = new URLSearchParams();
     if (options) {
