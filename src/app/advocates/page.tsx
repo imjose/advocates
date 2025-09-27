182cf5208a9827e41256daf69b71197e7d2cc4ae
Jose <jose@unioncredit.app>
2025-09-26 20:10:41 -0400

    searchCallback & resetCallback methods update to proper reflect functionallity.

--- src/app/advocates/page.tsx
+++ src/app/advocates/page.tsx
@@ -5,52 +5,37 @@ import { useState } from "react";
 import AdvocatesTable from '@/lib/components/advocatesTable';
 import SearchBar from '@/lib/components/searchBar';
 import { useAdvocates } from '@/lib/hooks/useAdvocates';
-import { PaginationOptions } from '@/lib/types/pagination';
+import { SearchPaginationParams } from '@/lib/types/params';
 
 export default function Home() {
-  const [paginationOptions, setPaginationOptions] = useState<PaginationOptions>({ page: 1, pageSize: 10 });
+  const [searchPagination, setSearchPagination] = useState<SearchPaginationParams>({ page: 1, pageSize: 10 });
   
-  const { data: advocates, loading } = useAdvocates(paginationOptions);
+  const { data: advocates, loading } = useAdvocates(searchPagination);
 
   const onPageChangeCallback = (page: number, pageSize: number) => {
-    setPaginationOptions({ page, pageSize });
+    setSearchPagination({ ...searchPagination, page, pageSize });
   };
 
-  const onChange = (e) => {
-    // const searchTerm = e.target.value;
-
-    // document.getElementById("search-term").innerHTML = searchTerm;
-
-    // console.log("filtering advocates...");
-    // const filteredAdvocates = advocates.filter((advocate) => {
-    //   return (
-    //     advocate.firstName.includes(searchTerm) ||
-    //     advocate.lastName.includes(searchTerm) ||
-    //     advocate.city.includes(searchTerm) ||
-    //     advocate.degree.includes(searchTerm) ||
-    //     advocate.specialties.includes(searchTerm) ||
-    //     advocate.yearsOfExperience.includes(searchTerm)
-    //   );
-    // });
-
-    // setFilteredAdvocates(filteredAdvocates);s
+  const onSearchCallback = (query: string) => {
+    setSearchPagination({ ...searchPagination, query });
   };
 
-  const onClick = () => {
-    // console.log(advocates);
-    // setFilteredAdvocates(advocates);
+  const onResetCallback = () => {
+    setSearchPagination({ ...searchPagination, query: '' });
   };
 
   return (
     <main className='my-6 mx-12'>
         <div className='flex flex-col items-start gap-4'>
-            <SearchBar onChange={onChange} onClick={onClick} loading={loading} />
-            <AdvocatesTable 
-                tableData={advocates} 
-                loading={loading} 
-                paginationOptions={paginationOptions} 
-                onPageChange={onPageChangeCallback} 
-            />
+            <SearchBar onSearch={onSearchCallback} onReset={onResetCallback} loading={loading} />
+            <div className='w-full'>
+                <AdvocatesTable 
+                    tableData={advocates} 
+                    loading={loading} 
+                    paginationOptions={searchPagination} 
+                    onPageChange={onPageChangeCallback} 
+                />
+            </div>
         </div>
     </main>
   );
